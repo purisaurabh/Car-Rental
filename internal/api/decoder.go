98092c3d40e83e97de989d7db5927645695ebc9e
Saurabh Puri <purisaurabh2885@gmail.com>
2024-11-15 12:45:08 +0530

    decode the user request

---
+++ internal/api/decoder.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/purisaurabh/car-rental/internal/pkg/specs"
+)
+
+func decodeUserRegistrationRequest(r *http.Request) (specs.UserRegistrationRequest, error) {
+	var req specs.UserRegistrationRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		fmt.Println("Error while decoding the user registration request : ", err)
+		return specs.UserRegistrationRequest{}, err
+	}
+	return req, nil
+}
+
+func decodeUserLoginRequest(r *http.Request) (specs.UserLoginRequest, error) {
+	var req specs.UserLoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		fmt.Println("Error while decoding the user login request : ", err)
+		return specs.UserLoginRequest{}, err
+	}
+	return req, nil
+}

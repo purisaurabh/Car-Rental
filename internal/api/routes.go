16d58dde6293bda9af65d6eaba759a684d07d5bc
Saurabh Puri <purisaurabh2885@gmail.com>
2024-11-15 12:45:26 +0530

    add user registration and login route

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/purisaurabh/car-rental/internal/app"
@@ -9,5 +10,7 @@ import (
 
 func Routes(ctx context.Context, svc app.Service) *mux.Router {
 	router := mux.NewRouter()
+	router.HandleFunc("/registration", UserRegistration(ctx, svc)).Methods(http.MethodPost)
+	router.HandleFunc("/login", UserLogin(ctx, svc)).Methods(http.MethodPost)
 	return router
 }
